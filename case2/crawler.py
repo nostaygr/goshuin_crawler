7c09b0ca64b94791255cf0af85f8327cfbd36333
Aiki YOUGAI <phoenix22.you@gmail.com>
2017-06-11 10:34:13 +0900

    case2から関数を切り出す

--- case2/crawler.py
+++ case2/crawler.py
@@ -5,136 +5,50 @@ import requests
 from bs4 import BeautifulSoup
 sys.path.append("lib")
 import api
+import crawl_wiki
 
 
 URL_BASE = "https://ja.wikipedia.org"
-WORSHIP_JUDGE_LIST = ["寺", "院", "師"]
 IS_TEMPLE = "1"
-
-
-# wikipediaのinfobox欄をparse
-def parse_worship_page(link):
-    url = URL_BASE + link
-    res = requests.get(url)
-    res.encoding = res.apparent_encoding
-    soup = BeautifulSoup(res.text, "html.parser")
-
-    try:
-        table = soup.find("table", {"class": "infobox"})
-        base_title = table.find("tr").find("th")
-    except:
-        print "can't find infobox"
-        raise
-    decision_char = base_title.text[-1].encode('utf-8')
-    if decision_char not in WORSHIP_JUDGE_LIST:
-        print "this is not worship page"
-        raise
-
-    title = base_title.text.encode('utf-8').replace('\n', '')
-    rows = table.find_all("tr")
-    for row in rows:
-        if not row.find("th"):
-            continue
-        th_text = row.find("th").text.encode('utf-8')
-        if th_text == "所在地":
-            address = row.find("td").text.encode('utf-8').replace('\n', '')
-        if th_text == "寺格":
-            rank = row.find("td").text.encode('utf-8').replace('\n', ',')
-        if th_text == "宗派" or th_text == "宗旨":
-            sect = row.find("td").text.encode('utf-8').replace('\n', ',')
-        if th_text == "本尊":
-            obj = row.find("td").text.encode('utf-8').replace('\n', ',')
-
-    try:
-        address
-    except:
-        address = ""
-
-    try:
-        rank
-    except:
-        rank = ""
-
-    try:
-        sect
-    except:
-        sect = ""
-
-    try:
-        obj
-    except:
-        obj = ""
-
-    try:
-        lat, lng = api.convert_address_to_latlng(address)
-    except:
-        lat = lng = 0
-
-    info = {}
-    info["title"] = title
-    info["address"] = address
-    info["lat"] = str(lat)
-    info["lng"] = str(lng)
-    info["rank"] = rank
-    info["sect"] = sect
-    info["obj"] = obj
-
-    return info
 
 
 def main():
     # get base url
     url = URL_BASE + "/wiki/日本の寺院一覧"
-    res = requests.get(url)
-    res.encoding = res.apparent_encoding
-    soup = BeautifulSoup(res.text, "html.parser")
+    temple_link_dic = crawl_wiki.get_links_from_list_page(url)
 
-    # parse worship list
-    content = soup.find("div", {"id": "mw-content-text"})
-    a_tag_list = content.find_all("a")
-
-    # parse worship link
-    worship_link_dic = {}
-    for a_tag in a_tag_list:
-        if not a_tag.text:
-            continue
-        try:
-            link = a_tag.get("href").encode('utf-8')
-            worship_name = a_tag.text.encode('utf-8')
-            worship_link_dic[worship_name] = link
-        except:
-            continue
-
-    # parse indivisual worship page
+    # parse indivisual temple page
     num = 1
-    len_worship_link_dic = len(worship_link_dic)
-    worship_dic = {}
-    for worship in worship_link_dic:
+    len_temple_link_dic = len(temple_link_dic)
+    temple_dic = {}
+    for temple in temple_link_dic:
         try:
-            worship_info = parse_worship_page(worship_link_dic[worship])
-            worship_dic[worship] = {}
-            worship_dic[worship]["info"] = worship_info
+            temple_info = crawl_wiki.parse_temple_page(temple_link_dic[temple])
+            temple_dic[temple] = {}
+            temple_dic[temple]["info"] = temple_info
 
-            print "%d/%d parse成功 : %s" % (num, len_worship_link_dic, worship)
+            print "%d/%d parse成功 : %s" % (num, len_temple_link_dic, temple)
         except:
-            print "%d/%d parse失敗 : %s" % (num, len_worship_link_dic, worship)
+            print "%d/%d parse失敗 : %s" % (num, len_temple_link_dic, temple)
 
         num += 1
 
-    # output worship_places data
+    print temple_dic
+
+    # output temple_places data
     output_fn = 'res/temples_base'
     with open(output_fn, 'w') as f:
-        for worship in worship_dic:
+        for temple in temple_dic:
             f.write(
-                worship_dic[worship]["info"]["title"] + ',' + \
-                worship_dic[worship]["info"]["title"] + ',' + \
+                temple_dic[temple]["info"]["title"] + ',' + \
+                temple_dic[temple]["info"]["title"] + ',' + \
                 IS_TEMPLE + ',' + \
-                worship_dic[worship]["info"]["address"] + ',' + \
-                worship_dic[worship]["info"]["lat"] + ',' + \
-                worship_dic[worship]["info"]["lng"] + ',' + \
-                worship_dic[worship]["info"]["sect"] + ',' + \
-                worship_dic[worship]["info"]["rank"] + ',' + \
-                worship_dic[worship]["info"]["obj"] + '\n'
+                temple_dic[temple]["info"]["address"] + ',' + \
+                temple_dic[temple]["info"]["lat"] + ',' + \
+                temple_dic[temple]["info"]["lng"] + ',' + \
+                temple_dic[temple]["info"]["sect"] + ',' + \
+                temple_dic[temple]["info"]["rank"] + ',' + \
+                temple_dic[temple]["info"]["obj"] + '\n'
             )
 
 
---
+++ lib/crawl_wiki.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+
+import requests
+from bs4 import BeautifulSoup
+
+
+TEMPLE_JUDGE_LIST = ["寺", "院", "師"]
+URL_BASE = "https://ja.wikipedia.org"
+
+
+# wikipediaページから,urlを抽出
+def get_links_from_list_page(url):
+    res = requests.get(url)
+    res.encoding = res.apparent_encoding
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    # parse worship list
+    content = soup.find("div", {"id": "mw-content-text"})
+    a_tag_list = content.find_all("a")
+
+    # parse worship link
+    worship_link_dic = {}
+    for a_tag in a_tag_list:
+        if not a_tag.text:
+            continue
+        try:
+            link = a_tag.get("href").encode('utf-8')
+            worship_name = a_tag.text.encode('utf-8')
+            worship_link_dic[worship_name] = link
+        except:
+            continue
+   
+    return worship_link_dic
+
+
+# wikipediaのtempleページのinfobox欄をparse
+def parse_temple_page(link):
+    url = URL_BASE + link
+    res = requests.get(url)
+    res.encoding = res.apparent_encoding
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    try:
+        table = soup.find("table", {"class": "infobox"})
+        base_title = table.find("tr").find("th")
+    except:
+        print "can't find infobox"
+        raise
+    decision_char = base_title.text[-1].encode('utf-8')
+    if decision_char not in TEMPLE_JUDGE_LIST:
+        print "this is not temple page"
+        raise
+
+    title = base_title.text.encode('utf-8').replace('\n', '')
+    rows = table.find_all("tr")
+    for row in rows:
+        if not row.find("th"):
+            continue
+        th_text = row.find("th").text.encode('utf-8')
+        if th_text == "所在地":
+            address = row.find("td").text.encode('utf-8').replace('\n', '')
+        if th_text == "寺格":
+            rank = row.find("td").text.encode('utf-8').replace('\n', ',')
+        if th_text == "宗派" or th_text == "宗旨":
+            sect = row.find("td").text.encode('utf-8').replace('\n', ',')
+        if th_text == "本尊":
+            obj = row.find("td").text.encode('utf-8').replace('\n', ',')
+
+    try:
+        address
+    except:
+        address = ""
+
+    try:
+        rank
+    except:
+        rank = ""
+
+    try:
+        sect
+    except:
+        sect = ""
+
+    try:
+        obj
+    except:
+        obj = ""
+
+    try:
+        lat, lng = api.convert_address_to_latlng(address)
+    except:
+        lat = lng = 0
+
+    info = {}
+    info["title"] = title
+    info["address"] = address
+    info["lat"] = str(lat)
+    info["lng"] = str(lng)
+    info["rank"] = rank
+    info["sect"] = sect
+    info["obj"] = obj
+
+    return info
