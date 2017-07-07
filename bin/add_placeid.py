b736496aeb66075eddeca6b0823a833689fb4dd0
Aiki YOUGAI <phoenix22.you@gmail.com>
2017-07-07 22:48:55 +0900

    rankファイル追加 & rankを置き換えるメソッドを追加

--- bin/add_placeid.py
+++ bin/add_placeid.py
@@ -9,17 +9,33 @@ def read_files(path):
     for file_path in files_path:
         with open(file_path, 'r') as f:
             for line in f:
-                lst.append(line.rstrip())
+                lst.append(line.rstrip().split(','))
     return lst
+
+
+def read_rank_file(path):
+    dic = {}
+    with open(path, 'r') as f:
+        for line in f:
+            item = line.rstrip().split(',')
+            dic[item[1]] = item[0]
+    return dic
+
+
+def replace_rank(worship_lst, rank_dic):
+    for idx, worship in enumerate(worship_lst):
+        if worship[0] in rank_dic:
+            worship_lst[idx][6] = rank_dic[worship[0]]
+    return worship_lst
 
 
 def add_placeid(shrines_lst, temples_lst):
     place_id = 1
     for idx, shrine in enumerate(shrines_lst):
-        shrines_lst[idx] = str(place_id) + ',' + shrine
+        shrines_lst[idx].insert(0, str(place_id))
         place_id += 1
     for idx, temple in enumerate(temples_lst):
-        temples_lst[idx] = str(place_id) + ',' + temple
+        temples_lst[idx].insert(0, str(place_id))
         place_id += 1
 
 
@@ -27,35 +43,38 @@ def main():
     shrines_lst = read_files('res/shrines_*')
     temples_lst = read_files('res/temples_*')
 
-    output_fn = 'res/worship_place'
+    rank_dic = read_rank_file('res/rank_shrines')
+    shrines_lst = replace_rank(shrines_lst, rank_dic)
+
     add_placeid(shrines_lst, temples_lst)
+    output_fn = 'res/worship_place'
     with open(output_fn, 'w') as f:
         for shrine in shrines_lst:
             f.write('{0}\n'.format(
-                ','.join([str(i) if i != "" else "" for i in shrine.split(',')[0:7]])
+                ','.join([str(i) if i != "" else "" for i in shrine[0:7]])
             ))
         for temples in temples_lst:
             f.write('{0}\n'.format(
-                ','.join([str(i) for i in temples.split(',')[0:7]])
+                ','.join([str(i) if i != "" else "" for i in temples[0:7]])
             ))
 
     output_fn = 'res/shrines'
     with open(output_fn, 'w') as f:
         for shrine in shrines_lst:
             f.write('{0},{1},{2}\n'.format(
-                shrine.split(',', -1)[0],
-                shrine.split(',', -1)[7],
-                shrine.split(',', -1)[8]
+                shrine[0],
+                shrine[7],
+                shrine[8]
             ))
     
     output_fn = 'res/temples'
     with open(output_fn, 'w') as f:
         for temple in temples_lst:
             f.write('{0},{1},{2},{3}\n'.format(
-                temple.split(',', -1)[0],
-                temple.split(',', -1)[7],
-                temple.split(',', -1)[8],
-                temple.split(',', -1)[9]
+                temple[0],
+                temple[7],
+                temple[8],
+                temple[9]
             ))
 
 
